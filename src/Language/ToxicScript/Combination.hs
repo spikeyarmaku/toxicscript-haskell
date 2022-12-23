b6d1edf03a11a9e5ae2dc7c394178d579fe5d8db
spikeyarmaku <spikeyarmaku@gmail.com>
2022-12-23 16:37:26 +0100

    Rename getParams to getParam

--- src/Language/ToxicScript/Combination.hs
+++ src/Language/ToxicScript/Combination.hs
@@ -6,12 +6,12 @@ getCombiner :: Expr -> Either String Expr
 getCombiner (List xs) | not (null xs) = Right . List $ init xs
 getCombiner expr = Left $  "`" ++ show expr ++ "` is not a valid combination."
 
-getParams :: Expr -> Either String Expr
-getParams (List xs) | not (null xs) = Right $ last xs
-getParams expr = Left $  "`" ++ show expr ++ "` is not a valid combination."
+getParam :: Expr -> Either String Expr
+getParam (List xs) | not (null xs) = Right $ last xs
+getParam expr = Left $  "`" ++ show expr ++ "` is not a valid combination."
 
 getCombination :: Expr -> Either String (Expr, Expr)
-getCombination expr = (,) <$> getCombiner expr <*> getParams expr
+getCombination expr = (,) <$> getCombiner expr <*> getParam expr
 
 getSymbol :: Expr -> Either String Symbol
 getSymbol (Atom a) = Right a
